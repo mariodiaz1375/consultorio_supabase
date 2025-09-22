3c091f36df1471c8eaa6f43640bc7e0556098479
mario <mariodiaz1375@gmail.com>
2025-09-22 18:23:30 -0300

    se creo funcion pacienteslist en pacienteslist.jsx

--- frontend/src/components/pacientesList/PacientesList.jsx
+++ frontend/src/components/pacientesList/PacientesList.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PacienteCard from '../pacienteCard/PacienteCard'
 import { getPacientes } from '../../api/pacientes.api'
 
 export default function PacientesList() {
-    
+  const [pacientes, setPacientes] = useState([])
+
+  useEffect(() => {
+    const fetchPacientes = async () => {
+      const data = await getPacientes()
+      setPacientes(data)
+    }
+    fetchPacientes()
+  }, [])
+  
   return (
     <div>PacientesList</div>
   )
