d44784ce2923b29f7bbfdba23d7ec84441e92c8b
Marcelo Diaz <marcelodiaz1375@gmail.com>
2025-11-16 12:57:16 -0300

    pantalla de auditoria de turnos implementada

--- turnos/views.py
+++ turnos/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import generics, status
 from django.shortcuts import get_object_or_404
 from django.db import IntegrityError
-from .models import Turnos, EstadosTurnos, HorarioFijo, DiaSemana 
+from .models import Turnos, EstadosTurnos, HorarioFijo, DiaSemana, AuditoriaTurnos
 from .serializers import (
     TurnosSerializer, 
     EstadosTurnosSerializer, 
